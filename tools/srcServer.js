eb8ee82ab71a02746cd1a8113b4dd5edcbddfffb
saikatcts <Saikat.Bhattacharya5@cognizant.com>
2017-01-03 00:56:22 +0530

    port changed

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -7,7 +7,7 @@ import webpackMiddleWare from 'webpack-dev-middleware';
 
 /* eslint-disable no-console */
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const compiler = webpack(config);
 
